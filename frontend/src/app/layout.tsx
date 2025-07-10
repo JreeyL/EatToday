b976a0f642cd615b4e3bb81519b4b367e086c77f
jiyu li <a00316304@student.tus.ie>
2025-07-10 22:40:01 +0100

    fix: 修复前端代码ESLint格式错误

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,11 @@
 // src/app/layout.tsx
+import React from 'react';
 import './globals.css';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
